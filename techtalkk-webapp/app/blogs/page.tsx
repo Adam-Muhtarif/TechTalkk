1e2d150c9727cf609a7daf37a1c0872125eadaa4
kadar naruto <khadarpaashe123@gmail.com>
2025-03-22 22:10:27 +0300

    setted up the folders for the pages such as events page, etc

---
+++ techtalkk-webapp/app/blogs/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// this is the blogs page
+// url route will be /blogs
+// this is also server component
+export default function page() {
+  return (
+    <div>page</div>
+  )
+}
---
+++ techtalkk-webapp/app/events/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+// here is the where the events will be displayed 
+// url route will be /events
+// this is server component and will be rendered on the server
+export default function page() {
+  return (
+    <div>events</div>
+  )
+}
