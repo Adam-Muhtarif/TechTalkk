3ce7b5de2cc4daf6643d98bbc78b0d27688aff4e
kadar naruto <khadarpaashe123@gmail.com>
2025-03-23 01:56:16 +0300

    added about tech-talk community section in about-us page

--- techtalkk-webapp/components/ui/about-us/about-us-hero.tsx
+++ techtalkk-webapp/components/ui/about-us/about-us-hero.tsx
@@ -1,28 +1,28 @@
-import Image from 'next/image'
-import React from 'react'
+import React from "react";
 
 export default function AboutUsHhero() {
   return (
-    <div className="relative max-w-[1440px]">
-    <div className="flex flex-col absolute top-1.5 left-26 items-start max-w-5xl mt-20 justify-start text-white z-1">
-      <h1 className="text-6xl sm:text-5xl max-w-5xl font-bold text-start text-white">
-        Knowledge Shared, Knowledge Gained
-      </h1>
-      <p className="mt-6 max-w-2xl text-start text-xl">
-        At TechTalk, we believe in the power of shared learning. Join our
-        community to explore industry trends, exchange insights, and expand
-        your tech expertise
-      </p>
-    </div>
+    <div className="relative max-w-[1440px] overflow-x-hidden">
+      <div className="absolute inset-x-0 top-1.5 lg:left-26 sm:mt-20 flex flex-col items-center sm:items-start max-w-5xl mx-auto lg:mx-0 z-1 ">
+        <h1 className="text-center text-2xl lg:text-6xl sm:text-3xl max-w-2xl px-1 sm:px-0 lg:max-w-5xl mx-auto lg:mx-0 font-bold lg:text-start text-white mt-[20%] sm:mt-0">
+          Knowledge Shared, Knowledge Gained
+        </h1>
+        <p className="text-center mt-6 max-w-2xl lg:max-w-5xl px-2 sm:px-0 mx-auto lg:text-start text-xl text-white">
+          At TechTalk, we believe in the power of shared learning. Join our
+          community to explore industry trends, exchange insights, and expand
+          your tech expertise
+        </p>
+      </div>
 
-    {/* image */}
-    <Image
-      src="/images/about-us/about-page-image.png"
-      width={1440}
-      height={460}
-      alt="About Us"
-    />
-    <div className="absolute inset-0 bg-[linear-gradient(to_left,rgba(0,0,0,0.6),rgba(0,0,0,0.9))]"></div>
-  </div>
-  )
+      {/* image */}
+      {/* <Image
+        src="/images/about-us/about-page-image.png"
+        width={1440}
+        height={460}
+        alt="About Us"
+      /> */}
+      <div className="w-[412px] h-[468px] sm:w-[1440px] sm:h-[460px] bg-[url(/images/about-us/about-page-image.jpeg)] bg-cover bg-center bg-no-repeat"></div>
+      <div className="absolute inset-0 bg-[linear-gradient(to_left,rgba(0,0,0,0.7),rgba(0,0,0,1.9))]"></div>
+    </div>
+  );
 }
