3e6d1b08a6ac7b9a7c4d9dca5747d7c8140b594a
juweria <jujumelmi143@gmail.com>
2025-03-22 22:07:28 +0300

    Re-added jobs collection

--- backend-payload/migrations/index.ts
+++ backend-payload/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20250317_143405 from './20250317_143405';
 import * as migration_20250318_131127 from './20250318_131127';
+import * as migration_20250322_190451 from './20250322_190451';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20250318_131127.up,
     down: migration_20250318_131127.down,
-    name: '20250318_131127'
+    name: '20250318_131127',
+  },
+  {
+    up: migration_20250322_190451.up,
+    down: migration_20250322_190451.down,
+    name: '20250322_190451'
   },
 ];
