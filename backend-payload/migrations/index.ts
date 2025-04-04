eaed618675bfebbb9bcb34d1d5d679e3bd36f10f
Adam Abdulkarim Adam <muhtarif.web.dev@gmail.com>
2025-04-04 23:19:34 +0300

    update: new fresh migrations folder

--- backend-payload/migrations/index.ts
+++ backend-payload/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20250404_195459 from './20250404_195459';
+import * as migration_20250404_201711 from './20250404_201711';
 
 export const migrations = [
   {
-    up: migration_20250404_195459.up,
-    down: migration_20250404_195459.down,
-    name: '20250404_195459'
+    up: migration_20250404_201711.up,
+    down: migration_20250404_201711.down,
+    name: '20250404_201711'
   },
 ];
