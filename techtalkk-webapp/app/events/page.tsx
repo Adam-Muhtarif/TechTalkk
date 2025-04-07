812a938b9efc5b6ae24d5fa20f185690a9e3cef8
kadar naruto <khadarpaashe123@gmail.com>
2025-04-07 10:52:44 +0300

    added event card

--- techtalkk-webapp/app/events/page.tsx
+++ techtalkk-webapp/app/events/page.tsx
@@ -1,11 +1,39 @@
+import HeroPage from '@/components/ui/about-us/hero-page'
+import JoinUs from '@/components/ui/about-us/join-us'
+import { Button } from '@/components/ui/button'
+import EventCard from '@/components/ui/events/event-card'
 import React from 'react'
 
-
-// here is the where the events will be displayed 
-// url route will be /events
-// this is server component and will be rendered on the server
 export default function page() {
   return (
-    <div>events</div>
+    <div className="containe mx-auto overflow-x-hidden font-geist-sans">
+      <HeroPage
+        header="Tech talk’s events"
+        paragraph="Home/events"
+        className="h-[468px] sm:h-[460px] bg-[url(/images/about-us/about-page-image.jpeg)] bg-cover bg-center bg-no-repeat"
+      />
+
+      <section className="max-w-7xl mx-auto py-24 relative px-4 sm:px-6 lg:px-8">
+        {/* filter buttons */}
+        {/* conatiner */}
+        <div className="flex gap-3 items-center justify-start">
+          {/* button upcoming*/}
+          <Button variant="upcoming">Upcoming</Button>
+
+          {/* button Nearby*/}
+          <Button variant="ghost" className="text-[#686868] bg-[#F2EEFF]">
+            Nearby
+          </Button>
+        </div>
+        {/* events */}
+        <EventCard />
+      </section>
+
+      <JoinUs
+        header="Don’t Miss Out!"
+        paragraph="Join the TeckTalkk community and take your skills to the next level. Whether you're here to learn, share, or connect, there's something for everyone. Let’s build the future of tech together!"
+        buttonText="Join"
+      />
+    </div>
   )
 }
