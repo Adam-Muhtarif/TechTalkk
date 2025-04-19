f7906efd76cf1952a73fca2ece5f730d00065757
kadar-naruto <khadarpaashe123@gmail.com>
2025-04-19 17:31:38 +0300

    changed the styling of the blue line that is crossing the landing page h1 and make it fit the center in a perfect way

--- techtalkk-webapp/components/shared/landingSection.tsx
+++ techtalkk-webapp/components/shared/landingSection.tsx
@@ -11,12 +11,14 @@ export default function LandingSection(props: LandingSectionProps) {
       className="uppercase text-center font-extrabold pt-44 pb-22 space-y-10"
     >
       <h3 className="text-xl md:text-2xl">{props.title}</h3>
-      <h1 className="text-6xl sm:text-7xl md:text-8xl !text-center items-center relative">
+      <div className="relative w-full">
+      <h1 className="text-6xl sm:text-7xl md:text-8xl !text-center items-center">
         {props.info}
-        <div
-          className={`absolute bg-linear-to-r from-[#4C0BF7] to-[#3105a0] rounded-r-2xl ${props.className}`}
-        ></div>
       </h1>
+      <div
+          className={`absolute hidden sm:block left-1/2 top-[55%] transform -translate-x-1/2 -translate-y-1/2  bg-linear-to-r from-[#4C0BF7] to-[#3105a0] ${props.className}`}
+      ></div>
+      </div>
     </section>
   )
 }
